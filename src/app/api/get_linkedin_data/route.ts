bf77253c36b8eb47e0bb5dc9cfcbe24a44eec219
seafood12 <abieledelso@gmail.com>
2024-01-20 14:39:40 +0000

    a

--- src/app/api/get_linkedin_data/route.ts
+++ src/app/api/get_linkedin_data/route.ts
@@ -1,6 +1,8 @@
+import { NextResponse } from 'next/server'
+
 export function GET (req: Request): any {
   console.log(req.body)
-  return Response.json({ text: '23' })
+  return NextResponse.json({ text: '23' })
 }
 
 export function POST (req: Request): any {
